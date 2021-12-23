8fad3b353f0fc04816388260a3b7d0a9fe558969
binilog <anos13@naver.com>
2021-12-23 12:24:18 +0900

    'pyweb'

--- board/urls.py
+++ board/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path('question/create/', views.question_create, name='question_create'),
     #답변 등록
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    #질문 수정
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify')
 ]
--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
 from board.models import Question, Answer
@@ -7,16 +8,21 @@ from board.forms import QuestionForm, AnswerForm
 
 def index(request):
     #질문 목록
-    question_list = Question.objects.all()  #db 전체조회
+    #question_list = Question.objects.all()  #db 전체조회
+    question_list = Question.objects.order_by('-create_date')
+    #작성일 기준 내림차순(- 기호 사용)
     return render(request, 'board/question_list.html',
                   {'question_list':question_list})
     #return HttpResponse("pyweb 사이트 입니다.")
 
 def detail(request, question_id):
     # 질문/답변 상세
-    question = Question.objects.get(id=question_id) #해당 id의 질문
+    # question = Question.objects.get(id=question_id) #해당 id의 질문
+    question = get_object_or_404(Question, pk=question_id)
+    #경로에 오류가 있을 때 404로 처리(페이지가 없음)
     return render(request, 'board/detail.html', {'question':question})
 
+@login_required(login_url='common:login')
 def question_create(request):
     #질문 등록
     if request.method == "POST":
@@ -24,20 +30,24 @@ def question_create(request):
         if form.is_valid():
             question = form.save(commit=False) #가저장(날짜가 없어서 가저장)
             question.create_date = timezone.now() #날짜 시간 저장
+            question.author = request.user  #글쓴이에 세션 저장
             question.save()  #실제 저장
             return redirect('board:index') #이동할 경로(앱 네임사용) 저장
     else:
         form = QuestionForm()   #form 객체 생성
     return render(request, 'board/question_form.html', {'form':form})
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     #답변 등록
-    question = Question.objects.get(id=question_id) #해당 id의 질문 객체 생성
+    #question = Question.objects.get(id=question_id) #해당 id의 질문 객체 생성
+    question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST) #입력값 전달받음
         if form.is_valid():
             answer = form.save(commit=False)  #내용만 저장됨
             answer.create_date = timezone.now() #작성일
+            answer.author = request.user #세션 발급
             answer.question = question  #외래키 질문 저장
             answer.save()
             return redirect('board:detail', question_id=question.id)
@@ -45,3 +55,25 @@ def answer_create(request, question_id):
         form= AnswerForm()
     context = {'question':question,'form':form}
     return render(request, 'board/detail.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('board:detail', question_id=question_id)
+        else:
+            form = QuestionForm(instance=question)
+            return render(request, 'board/question_form.html', {'form':form})
+
+        @login_required(login_url='common:login')
+        def question_delete(request, question_id):
+            question = get_object_or_404(Question, pk=question_id)
+            question.delete()
+            return redirect('board:index')
+
